a45becd1b9638e0def0310065e9abd51a1d370cc
Nishant Sharma <nishantsharma5330@gmail.com>
2026-03-30 21:27:01 +0530

    add index.js

--- backend/index.js
+++ backend/index.js
@@ -2,16 +2,21 @@ import express from "express"
 import dotenv from "dotenv"
 import userRoute from "./routes/userRoute.js"
 import databaseConnection from "./config/database.js";
+import cookieParser from "cookie-parser";
+import messageRoute from "./routes/messageRoute.js"
 const app = express();
 
 const PORT = process.env.PORT || 5000
 
 //middleware
 app.use(express.json());
+app.use(cookieParser());
 
 //routes
 app.use("/api/v1/user",userRoute)
-// http://localhost:8080/api/v1/user/register
+app.use("/api/v1/message", messageRoute)
+
+
 
 
 dotenv.config({
