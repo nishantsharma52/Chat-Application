94b5d2c6f31ba7ac0d95fccdcfc0b69fa686a84e
Nishant Sharma <nishantsharma5330@gmail.com>
2026-04-02 18:09:37 +0530

    update login , signup.jsx

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,5 +1,8 @@
 import React, { use, useState } from 'react'
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+import axios from "axios";
+import toast from "react-hot-toast"
+
 
 const Signup = () => {
   const [user, setUser] = useState({
@@ -9,12 +12,31 @@ const Signup = () => {
     confirmPassword: "",
     gender: "",
   })
+  const navigate = useNavigate();
   const handleCheckBox = (gender) => {
     setUser({ ...user, gender })
 
   }
-  const onSubmitHandler = (e) => {
+  const onSubmitHandler =  async (e) => {
     e.preventDefault()
+    try {
+      console.log(user);
+      
+      const res = await axios.post(`http://localhost:8080/api/v1/user/register`, user, {
+        headers :{
+          "Content-Type":'application/json'
+        },
+        withCredentials:true
+      })
+      if(res.data.success) {
+        navigate("/login")
+        toast.success(res.data.message);
+      }
+      
+    } catch (error) {
+       toast.error(error.response.data.message)
+      console.log(error);
+    }
     setUser({
       fullName: "",
       username: "",
