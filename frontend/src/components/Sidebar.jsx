dc11209551e4967205ad15db9754e498604eee78
Nishant Sharma <nishantsharma5330@gmail.com>
2026-04-03 09:29:36 +0530

    updated code

---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { BiSearchAlt2 } from "react-icons/bi";
+import OtherUsers from './OtherUsers';
+
+const Sidebar = () => {
+    return (
+        <div className='borde-r border-slate-500 p-4 flex flex-col'>
+            <form action="" className='flex items-center gap-2'>
+                <input
+                    className='input input-bordered rounded-md outline-none bg-white' type="text"
+                    placeholder='Search...'
+                />
+                <button type='submit' className='btn  bg-zinc-700'>
+                    <BiSearchAlt2 className='hover:invert w-6 h-6 outline-none' />
+                </button>
+            </form>
+            <div className='divider px-3'> </div>
+            <OtherUsers/>
+            <div className='mt-2'>
+                <button className='btn btn-sm '>Logout</button>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,99 +1,100 @@
-import React, { use, useState } from 'react'
+
+import React, { useState } from 'react'
 import { Link, useNavigate } from "react-router-dom"
-import axios from "axios";
+import axios from "axios"
 import toast from "react-hot-toast"
 
 
 const Signup = () => {
-  const [user, setUser] = useState({
-    fullName: "",
-    username: "",
-    password: "",
-    confirmPassword: "",
-    gender: "",
-  })
-  const navigate = useNavigate();
-  const handleCheckBox = (gender) => {
-    setUser({ ...user, gender })
-
-  }
-  const onSubmitHandler =  async (e) => {
-    e.preventDefault()
-    try {
-      console.log(user);
-      
-      const res = await axios.post(`http://localhost:8080/api/v1/user/register`, user, {
-        headers :{
-          "Content-Type":'application/json'
-        },
-        withCredentials:true
-      })
-      if(res.data.success) {
-        navigate("/login")
-        toast.success(res.data.message);
-      }
-      
-    } catch (error) {
-       toast.error(error.response.data.message)
-      console.log(error);
-    }
-    setUser({
-      fullName: "",
-      username: "",
-      password: "",
-      confirmPassword: "",
-      gender: "",
+    const [user, setUser] = useState({
+        fullName: "",
+        username: "",
+        password: "",
+        confirmPassword: "",
+        gender: ""
     })
+    const navigate = useNavigate()
+    const handleCheckbox = (gender) => {
+        setUser({ ...user, gender })
+    }
+    const onSubmitHandler = async (e) => {
+        e.preventDefault()
+        try {
+            console.log(user)
+            const res = await axios.post(`http://localhost:8080/api/v1/user/register`, user, {
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                withCredentials: true
+            })
+            if (res.data.success) {
+                navigate("/login")
+                toast.success(res.data.message)
 
-  }
-  return (
-    <div className='min-w-96 mx-auto'>
-      <div className='w-full p-6 rounded-lg shadow-md bg-white/10 backdrop-blur-md border border-white/20'>
-        <h1 className='text-3xl font-bold text-center'>Signup</h1>
-        <form onSubmit={onSubmitHandler} action="">
-          <div>
-            <label className='label p-2'>
-              <span className='text-base label-text '>Full Name </span>
-            </label>
-            <input value={user.fullName} onChange={(e) => setUser({ ...user, fullName: e.target.value })} className='w-full input input-borderd h-10' type="text" placeholder='Full Name' />
-          </div>
-          <div>
-            <label className='label p-2'>
-              <span className='text-base label-text '>Username </span>
-            </label>
-            <input value={user.username} onChange={(e) => setUser({ ...user, username: e.target.value })} className='w-full input input-borderd h-10' type="text" placeholder='username' />
-          </div>
-          <div>
-            <label className='label p-2'>
-              <span className='text-base label-text '>Password </span>
-            </label>
-            <input value={user.password} onChange={(e) => setUser({ ...user, password: e.target.value })} className='w-full input input-borderd h-10' type="password" placeholder='password' />
-          </div>
-          <div>
-            <label className='label p-2'>
-              <span className='text-base label-text '>Confirm-Password </span>
-            </label>
-            <input value={user.confirmPassword} onChange={(e) => setUser({ ...user, confirmPassword: e.target.value })} className='w-full input input-borderd h-10' type="password" placeholder='confirm-password' />
-          </div>
-          <div className=' flex items-center my-4'>
-            <div className='flex items-center'>
-              <p>Male</p>
-              <input checked={user.gender === "male"} onChange={() => handleCheckBox("male")} type="checkbox" defaultChecked className="checkbox mx-2 checkbox-neutral" />
-            </div>
-            <div className=' flex items-center'>
-              <p>Female</p>
-              <input checked={user.gender === "female"} onChange={() => handleCheckBox("female")} type="checkbox" defaultChecked className="checkbox mx-2 checkbox-neutral" />
-            </div>
-          </div>
-          <p className=' text-center my-2 '> Already have an account?  <Link to="/login"> login</Link> </p>
-          <div>
-            <button type='submit' className='btn btn-block btn-sm mt-2 border border-slate-700'>Signup</button>
-          </div>
+            }
 
-        </form>
-      </div>
-    </div>
-  )
+        } catch (error) {
+            toast.error(error.response.data.message)
+
+            console.log(error)
+        }
+        setUser({
+            fullName: "",
+            username: "",
+            password: "",
+            confirmPassword: "",
+            gender: ""
+        })
+
+    }
+    return (
+        <div className='min-w-96 mx-auto'>
+            <div className='w-full p-6 rounded-lg shadow-md bg-white/10 backdrop-blur-md border border-white/20'>
+                <h1 className='text-3xl font-bold text-center text-black '>Signup</h1>
+                <form onSubmit={onSubmitHandler} action="">
+                    <div>
+                        <label className='label p-2 '>
+                            <span className='text-base label-text text-black '>Full Name</span>
+                        </label>
+                        <input value={user.fullName} onChange={(e) => setUser({ ...user, fullName: e.target.value })} className='w-full input input-bordered h-10 text-black bg-white' type="text" placeholder='FullName' />
+                    </div>
+                    <div>
+                        <label className='label p-2 '>
+                            <span className='text-base label-text text-black '>Username</span>
+                        </label>
+                        <input value={user.username} onChange={(e) => setUser({ ...user, username: e.target.value })} className='w-full input input-bordered h-10 text-black bg-white' type="text" placeholder='Username' />
+                    </div>
+                    <div>
+                        <label className='label p-2 '>
+                            <span className='text-base label-text text-black '>Password</span>
+                        </label>
+                        <input value={user.password} onChange={(e) => setUser({ ...user, password: e.target.value })} className='w-full input input-bordered h-10 text-black bg-white' type="password" placeholder='Password' />
+                    </div>
+                    <div>
+                        <label className='label p-2 '>
+                            <span className='text-base label-text text-black '>Confirm Password</span>
+                        </label>
+                        <input value={user.confirmPassword} onChange={(e) => setUser({ ...user, confirmPassword: e.target.value })} className='w-full input input-bordered h-10 text-black bg-white' type="password" placeholder='Confirm Password' />
+                    </div>
+                    <div className='flex items-center my-4'>
+                        <div className='flex items-center'>
+                            <p className='text-black'>Male</p>
+                            <input checked={user.gender === "male"} onChange={() => handleCheckbox("male")} type="checkbox" className="checkbox checkbox-neutral mx-2" />
+                        </div>
+                        <div className='flex items-center'>
+                            <p className='text-black'>Female</p>
+                            <input checked={user.gender === "female"} onChange={() => handleCheckbox("female")} type="checkbox" className="checkbox checkbox-neutral mx-2" />
+                        </div>
+                    </div>
+                    <p className='text-center my-2 text-black'>Already have an account? <Link to="/login">Login</Link></p>
+                    <div >
+                        <button type='submit' className='btn btn-block btn-md mt-2 text-black text-lg bg-white border-slate-700 '>Signup</button>
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    )
 }
 
 export default Signup
