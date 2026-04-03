856daa04be062125b725c2aef34882b2371fed52
Nishant Sharma <nishantsharma5330@gmail.com>
2026-04-03 09:28:24 +0530

    updated code

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Sidebar from './Sidebar'
+import MessageContainer from './MessageContainer'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div className='flex sm-[450px] md:h-[550px] text-black rounded-lg overflow-hidden bg-gray-400 shadow-md bg-white/10 backdrop-blur-md border border-white/20'>
+      <Sidebar/>
+      <MessageContainer/>
+    </div>
   )
 }
 
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,65 +1,67 @@
+
 import React, { useState } from 'react'
-import { Link,useNavigate } from "react-router-dom"
-import toast from 'react-hot-toast'
+import { Link, useNavigate } from "react-router-dom"
 import axios from 'axios'
+import toast from 'react-hot-toast'
+import { useDispatch } from 'react-redux'
+import { setAuthUser } from '../redux/userSlice'
 
 const Login = () => {
   const [user, setUser] = useState({
     username: "",
     password: "",
   })
+  const dispatch = useDispatch()
   const navigate = useNavigate()
-
-  const onSubmitHandler =  async (e) => {
+  const onSubmitHandler = async (e) => {
     e.preventDefault()
-     try {
-      console.log(user);
-      
-      const res = await axios.post(`http://localhost:8080/api/v1/user/login`, user, {
-        headers :{
-          "Content-Type":'application/json'
+    try {
+      const res = await axios.post("http://localhost:8080/api/v1/user/login", user, {
+        headers:{
+          "Content-Type": "application/json"
         },
         withCredentials:true
       })
-        navigate("/")
-        console.log(res);
-        
+      toast.success("logged in sucessfully")
+      navigate("/homepage")
+      console.log(res.data)
+      dispatch(setAuthUser(res.data))
     } catch (error) {
       toast.error(error.response.data.message)
-      console.log(error);
+      console.log(error)
     }
+    
     setUser({
       username: "",
       password: "",
 
     })
-
   }
   return (
     <div className='min-w-96 mx-auto'>
       <div className='w-full p-6 rounded-lg shadow-md bg-white/10 backdrop-blur-md border border-white/20'>
-        <h1 className='text-3xl font-bold text-center'>Login</h1>
+        <h1 className='text-3xl font-bold text-center text-black '>Login</h1>
         <form onSubmit={onSubmitHandler} action="">
+
           <div>
-            <label className='label p-2'>
-              <span className='text-base label-text '>Username </span>
+            <label className='label p-2 '>
+              <span className='text-base label-text text-black '>Username</span>
             </label>
-            <input value={user.username} onChange={(e) => setUser({ ...user, username: e.target.value })} className='w-full input input-borderd h-10' type="text" placeholder='username' />
+            <input value={user.username} onChange={(e) => setUser({ ...user, username: e.target.value })} className='w-full input input-bordered h-10 text-black bg-white' type="text" placeholder='Username' />
           </div>
           <div>
-            <label className='label p-2'>
-              <span className='text-base label-text '>Password </span>
+            <label className='label p-2 '>
+              <span className='text-base label-text text-black '>Password</span>
             </label>
-            <input value={user.password} onChange={(e) => setUser({ ...user, password: e.target.value })} className='w-full input input-borderd h-10' type="password" placeholder='password' />
+            <input value={user.password} onChange={(e) => setUser({ ...user, password: e.target.value })} className='w-full input input-bordered h-10 text-black bg-white' type="password" placeholder='Password' />
           </div>
 
-
-          <p className=' text-center my-2 '> Don't have an account?  <Link to="/register"> signup</Link> </p>
-          <div>
-            <button type='submit' className='btn btn-block btn-sm mt-2 border border-slate-700'>Login</button>
+          <p className='text-center my-2 text-white'>Don't have an account? <Link to="/">Signup</Link></p>
+          <div >
+            <button type='submit' className='btn btn-block btn-md mt-2 text-black text-lg bg-white border-slate-700 '>Login</button>
           </div>
-
         </form>
+
       </div>
     </div>
   )
