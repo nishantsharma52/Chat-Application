fd326f5417aea10e7b1d2d1d01dd2eb57b2c7cae
Nishant Sharma <nishantsharma5330@gmail.com>
2026-04-04 14:20:28 +0530

    updated code

--- frontend/src/components/OtherUsers.jsx
+++ frontend/src/components/OtherUsers.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
 import OtherUser from './OtherUser'
+import useGetOtherUsers from '../hooks/useGetOtherUsers'
+import { useSelector } from 'react-redux'
 
 const OtherUsers = () => {
+   // my custom hook
+  useGetOtherUsers()
+const {otherUsers} = useSelector(store=>store.user)
+if (!otherUsers) return;
   return (
-    <div className='overflow-auto'>
-      <OtherUser/>
-      <OtherUser/>
-      <OtherUser/>
-      <OtherUser/>
-      <OtherUser/>
-      <OtherUser/>
-      <OtherUser/>
-      <OtherUser/>
-      <OtherUser/>
-      <OtherUser/>
+    <div className='overflow-auto flex-1'>
+      {
+        otherUsers?.map((user) =>{
+          return(
+              <OtherUser key={user._id} user={user}/>
+          )
+        })
+      }
+    
+    
     </div>
   )
 }
