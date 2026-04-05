05f68b827594eb090c6b16d90449e978d49827bc
Nishant Sharma <nishantsharma5330@gmail.com>
2026-04-05 13:57:19 +0530

    updated code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,59 @@
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Signup from "./components/Signup"
 import HomePage from "./components/HomePage"
 import Login from "./components/Login"
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import io from "socket.io-client"
+import { useState } from "react";
+import { setOnlineUsers } from "./redux/userSlice";
 
 const router = createBrowserRouter([
   {
-    path:"/",
-    element:<HomePage/>
+    path: "/",
+    element: <HomePage />
   },
   {
-    path:"/register",
-    element:<Signup/>
+    path: "/register",
+    element: <Signup />
   },
   {
-    path:"/login",
-    element:<Login/>
+    path: "/login",
+    element: <Login />
   },
 
 ])
 function App() {
+  const { authUser } = useSelector(store => store.user)
+  const { socket } = useSelector(store => store.socket)
+  const dispatch = useDispatch()
 
+  useEffect(() => {
+
+    if (authUser) {
+      const socket = io('http://localhost:8080', {
+        query:{
+          userId:authUser._id
+        }
+
+      });
+      dispatch(setSocket(socket))
+
+      socket.on('getOnlineUsers', (onlineUsers)=>{
+        dispatch(setOnlineUsers(onlineUsers))
+      })
+      return ()=> socket.close()
+    }else{
+      if(socket){
+        socket.close()
+        dispatch(setSocket(null))
+      }
+    }
+  }, [authUser])
   return (
     <div className="flex justify-center items-center h-screen p-4">
-      <RouterProvider router={router}/>
-    
+      <RouterProvider router={router} />
+
     </div>
   )
 }
