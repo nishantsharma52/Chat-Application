93a727e615e584bdc7c47f188b6de82bd8f58c01
Nishant Sharma <nishantsharma5330@gmail.com>
2026-04-13 08:53:07 +0530

    make responsive after deployment

--- frontend/src/components/OtherUser.jsx
+++ frontend/src/components/OtherUser.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { setSelectedUser } from '../redux/userSlice'
+import { setSelectedUser } from '../redux/userSlice';
 
 const OtherUser = ({ user }) => {
   const dispatch = useDispatch()
@@ -9,26 +9,18 @@ const OtherUser = ({ user }) => {
   const selectedUserHandler = (user) => {
     dispatch(setSelectedUser(user))
   }
-  console.log("online users",onlineUsers);
-  console.log(user._id);
-  
- 
-  
-  
+
   return (
     <>
-
       <div onClick={() => selectedUserHandler(user)} className={` ${selectedUser?._id === user?._id ? 'flex bg-white/20' : ''} flex gap-2 items-center hover:bg-white/20 rounded p-2 cursor-pointer`}>
         <div className={`avatar ${isOnline ? 'online' : ''}`}>
-          <div className='w-12 rounded-full'>
+          <div className='w-10 sm:w-12 rounded-full'>
             <img src={user?.profilePhoto} alt="user-profile" />
-           
-
           </div>
         </div>
-        <div className='flex flex-col flex-1'>
-          <div className='flex justify-between  gap-2 '>
-            <p>{user?.fullName}</p>
+        <div className='flex flex-col flex-1 min-w-0'>
+          <div className='flex justify-between gap-2'>
+            <p className='truncate '>{user?.fullName}</p>
           </div>
         </div>
       </div>
